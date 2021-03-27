7432aafd209378fa0bdf7c7ae06956d786c3b180
Egor Semenchenko <topmonroe9@gmail.com>
2021-03-28 02:22:40 +0300

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            var timer = new StopWatch();
+            var count = true;
+            while (count == true)
+            {
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "start":
+                        timer.Start();
+                        break;
+                    case "stop":
+                        timer.Stop();
+                        timer.Report();
+                        break;
+                    case "exit":
+                        count = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry. Try 'start', 'stop' or 'exit'");
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Practice/StopWatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan interval;
+        private bool hasStarted;
+        private bool hasEnded;
+        public void Start()
+        {
+            if (hasStarted == false)
+            {
+                startTime = DateTime.Now;
+                Console.WriteLine(startTime);
+                hasStarted = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if (hasStarted == true && hasEnded == false)
+            {
+                stopTime = DateTime.Now;
+                hasEnded = true;
+            }
+        }
+
+        public void Report()
+        {
+            
+            if (hasStarted == true && hasEnded == true)
+            {
+                interval = stopTime - startTime;
+                Console.WriteLine($"Time elapsed: {interval.Seconds}");
+                hasStarted = false;
+                hasEnded = false;
+            }
+        }
+
+    }
+}
