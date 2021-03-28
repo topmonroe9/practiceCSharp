a43d6ac51c1024aefe44c8dbf2837be3d3a8b80b
Egor Semenchenko <topmonroe9@gmail.com>
2021-03-28 04:04:28 +0300

    Added Stack practice

---
+++ StackWithObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ StackWithObjects/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace StackWithObjects
+{
+    class Stack
+    {
+        private ArrayList stack = new ArrayList();
+
+        public void Push(object obj)
+        {
+            stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            var lastInStack = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return lastInStack;
+        }
+
+    }
+}
